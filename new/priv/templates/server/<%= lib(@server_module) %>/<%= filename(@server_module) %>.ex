4dbbb3583c92bee55be4c772aeb3b38fdfab1ab8
Ian Luites <ian@luites.com>
2022-08-01 00:06:40 +0200

    🐛 fix server generation service module

--- new/priv/templates/server/<%= lib(@server_module) %>/<%= filename(@server_module) %>.ex
+++ new/priv/templates/server/<%= lib(@server_module) %>/<%= filename(@server_module) %>.ex
@@ -4,5 +4,5 @@ defmodule <%= inspect @server_module %> do
   """
   use Cordial.Server<%= if @grpc? do %>, grpc: true<% end %><%= if @http? do %>, http: true<% end %>
   <%= for service <- @services do %>
-  service <%= inspect service %>
+  service <%= inspect Module.concat(@server_module, service) %>
 <% end %>end
