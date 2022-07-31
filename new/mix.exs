9f19615ed6f95f9211eb91c78ad5979456ad5611
Ian Luites <ian@luites.com>
2022-07-31 14:44:42 +0200

    🔧 add skeleton cordial.new project

---
+++ new/mix.exs
@@ -0,0 +1,61 @@
+defmodule Cordial.New.MixProject do
+  use Mix.Project
+
+  @app :cordial_new
+  @version "0.0.1"
+  @scm_project "ianluites/cordial"
+  @scm_url "https://github.com/#{@scm_project}/#{@app}"
+
+  @external_resource readme = "./README.md"
+  @description readme
+               |> File.stream!([:read], :line)
+               |> Enum.take(3)
+               |> List.last()
+               |> String.trim_trailing(".\n")
+
+  def project do
+    [
+      app: @app,
+      version: @version,
+      description: @description,
+      elixir: "~> 1.13",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      package: package()
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Ian Luites"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @scm_url},
+      files: ~W(
+        mix.exs
+        .formatter.exs
+        lib
+        README.md
+        CHANGELOG.md
+        LICENSE.md
+      )
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:eex, :logger]
+    ]
+  end
+
+  defp deps do
+    mode =
+      if System.get_env("CORDIAL_LOCAL_DEPENDENCIES", "false") == "true",
+        do: [path: "../cordial"],
+        else: [github: @scm_project, sparse: "Cordial"]
+
+    [
+      {:cordial, @version, mode},
+      {:heimdallr, ">= 0.0.0", only: [:dev, :test]}
+    ]
+  end
+end
---
+++ new/test/cordial/new_test.exs
@@ -0,0 +1,4 @@
+defmodule Cordial.NewTest do
+  use ExUnit.Case
+  doctest Cordial.New
+end
